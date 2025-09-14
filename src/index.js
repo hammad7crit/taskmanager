24f886b5d9d38b457a00142ee4d972636d780135
hammad7crit <hammadali6942@gmal.com>
2025-09-14 14:07:09 +0500

    Dyanmic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+
 import "./styles.css";
 import "./main.css";
 import "./taskUI.css";
 let mainWindow = document.querySelector("#main");
+let sideBar = document.querySelector("#sidebar");
 let projectCreationUI = document.createElement("div");
 let closeProjectOptional = document.createElement("button");
 
@@ -11,6 +13,7 @@ const displayHeading = function () {
     topHeading.classList.add("top-heading");
     topHeading.innerText = projectButton.innerText;
     mainWindow.appendChild(topHeading);
+    return {topHeading, projectButton};
 };
     
 const displayAddButton = function () {
@@ -22,21 +25,18 @@ const displayAddButton = function () {
 
 
 const optionalProjectClose = () => {
-    let optionalCloseDiv = document.createElement("div");
-    optionalCloseDiv.classList.add("optional-close-div");
-    projectCreationUI.appendChild(optionalCloseDiv);
-
     closeProjectOptional.classList.add("closeProjectOP");
     closeProjectOptional.classList.add("fa-solid", "fa-xmark");
-    optionalCloseDiv.appendChild(closeProjectOptional);
+    projectCreationUI.appendChild(closeProjectOptional); 
 };
 
 const addNewProject = () => {
+    projectCreationUI.innerText = "Enter Project Name";
+    
     let buttonClicked = false;
     let addProjectButton = document.querySelector(".add-project-button");
     addProjectButton.addEventListener("click", () => {
         if (buttonClicked === false) {
-            buttonClicked === true;
             projectCreationUI.classList.add("project-create-ui");
             mainWindow.appendChild(projectCreationUI);
 
@@ -58,10 +58,58 @@ const addNewProject = () => {
         })
         
     });
+    return projectCreationUI;
+};
+
+const addProjectLayout = (projectCreationUI) => {
+    const projectCreateDiv = document.createElement("div");
+    const checkMark = document.createElement("i");
+    checkMark.classList.add("check-mark");
+
+    checkMark.classList.add("fa-solid", "fa-check");
+    projectCreateDiv.classList.add("project-create-div");
+
+    projectCreationUI.appendChild(projectCreateDiv);
+    const projectName = document.createElement("input");
+
+    projectName.classList.add("project-inputs");
+    projectCreateDiv.appendChild(projectName);
+    projectCreateDiv.appendChild(checkMark);
+    return {checkMark, projectName}
+};
+
+const submitProject = (checkMark, projectName, headingDisplay, projectButton) => {
+    checkMark.addEventListener("click", () => {
+        const newProjectCreated = document.createElement("button");
+        newProjectCreated.classList.add("new-project-created");
+        newProjectCreated.innerText = projectName.value; 
+
+        const buttons = sideBar.querySelectorAll("button");
+
+        if (buttons.length >= 2) {
+            sideBar.insertBefore(newProjectCreated, buttons[1]);
+        } else {
+            sideBar.appendChild(newProjectCreated);
+        }
+        newProjectCreated.addEventListener("click", () => {
+            headingDisplay.innerText = newProjectCreated.innerText;
+        });
+    });
+
+    projectButton.addEventListener("click", () => {
+        headingDisplay.innerText = projectButton.innerText;
+    });
 };
 
 
-addNewProject();
-displayHeading();
+
+
+
+
+
+const newProject = addNewProject();
+const {topHeading, projectButton} = displayHeading();
 displayAddButton();
 optionalProjectClose();
+const {checkMark, projectName} = addProjectLayout(newProject);
+submitProject(checkMark, projectName, topHeading, projectButton);
